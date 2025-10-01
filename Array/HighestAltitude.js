6f36d394085774793e11037d01bf37f5c8241aba
Manish Dhanuka <34171208+manishdhanuka@users.noreply.github.com>
2025-10-01 18:35:21 +0000

    Highest altitude

--- Array/HighestAltitude.js
+++ Array/HighestAltitude.js
@@ -1,1 +1,13 @@
+function largestAltitude(gain) {
+    let maxAltitude = 0;
+    let currentAltitude = 0;
+    for(let gainInAltitude of gain) {
+        currentAltitude += gainInAltitude;
+        maxAltitude = Math.max(currentAltitude, maxAltitude);
+    }
+    return maxAltitude;
+}
 
+console.log(largestAltitude([-5, 1, 5, 0, -7])); // 1
+console.log(largestAltitude([4, -3, 2, -1, -2])); // 4
+console.log(largestAltitude([2, 2, -3, -1, 2, 1, -5])); // 4
