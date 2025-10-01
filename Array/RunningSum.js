5512bd64f9c1e1d20c79fbcd5b8c3fb3299cd6fb
Manish Dhanuka <34171208+manishdhanuka@users.noreply.github.com>
2025-10-01 14:22:34 +0000

    return true if array contains duplicate

--- Array/RunningSum.js
+++ Array/RunningSum.js
@@ -5,4 +5,6 @@ function runningSum(nums) {
   return nums;
 }
 
-console.log(runningSum([2, 3, 5, 1, 6]));
+console.log(runningSum([2, 3, 5, 1, 6])); // [2, 5, 10, 11, 17]
+console.log(runningSum([1, 1, 1, 1, 1])); // [1, 2, 3, 4, 5]
+console.log(runningSum([-1, 2, -3, 4, -5])); // [-1, 1, -2, 2, -3]
